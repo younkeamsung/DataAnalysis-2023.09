4e82e0d5597e09b27234ff1188baeebff9a1d0be
younkeamsung <139431366+younkeamsung@users.noreply.github.com>
2023-09-27 10:51:29 +0900

    Prototype

---
+++ 08.Prototype/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, flash
+from bp.crawling import crawl_bp
+from bp.map import map_bp
+
+app = Flask(__name__)
+app.secret_key = 'qwert12345'       # flash와 session을 사용하려면 반드시 설정해야 함
+
+app.register_blueprint(crawl_bp, url_prefix='/crawling')    # localhost:5000/crawling/* 는 craw bp가 처리
+app.register_blueprint(map_bp, url_prefix='/map')
+
+@app.route('/')
+def home():
+    menu = {'ho':1, 'us':0, 'cr':0, 'ma':0, 'sc':0}
+    flash('Welcome to my Web!!!')
+    return render_template('home.html', menu=menu)
+
+@app.route('/schedule')
+def schedule():
+    menu = {'ho':0, 'us':0, 'cr':0, 'ma':0, 'sc':1}
+    return render_template('schedule.html', menu=menu)
+
+if __name__ == '__main__':
+    app.run(debug=True)
